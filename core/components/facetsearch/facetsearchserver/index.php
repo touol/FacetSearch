bc0acb039bf8355480218d4ec5e1f8b075335891
Александр Туниеков <touols@yandex.ru>
2023-04-06 05:42:41 +0700

    Добавлен код facetsearchserver

--- core/components/facetsearch/facetsearchserver/index.php
+++ core/components/facetsearch/facetsearchserver/index.php
@@ -1,7 +1,14 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+$config = [
+    'host'=>'https://localhost:9200',
+    'login'=>'admin',
+    'password'=>'password',
+    'index'=>'demo.loc',
+    'api_key'=>'',
+];
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/facet.class.php';
-$facet = new facet();
+$facet = new facet($config);
 echo json_encode($facet->run());
