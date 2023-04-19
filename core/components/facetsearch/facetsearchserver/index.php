07ad85fbe81b277cf017654c9258b554d80a142a
Александр Туниеков <touols@yandex.ru>
2023-04-19 13:59:01 +0700

    комментарии

--- core/components/facetsearch/facetsearchserver/index.php
+++ core/components/facetsearch/facetsearchserver/index.php
@@ -2,11 +2,11 @@
 // error_reporting(E_ALL);
 // ini_set('display_errors', 1);
 $config = [
-    'host'=>'https://localhost:9200',
-    'login'=>'admin',
-    'password'=>'password',
-    'index'=>'demo.loc',
-    'api_key'=>'',
+    'host'=>'https://localhost:9200',   //Адрес OpenSearch
+    'login'=>'admin',                   //Логин
+    'password'=>'admin',                //Пароль
+    'index'=>'demo.loc',                //Индекс базы(такой же как на вашем сайте в настройках FacetSearch)
+    'api_key'=>'',                      //Ключ АПИ. Сгенирируйте с помощью какого-либо генератора паролей. Его же забить в настройки FacetSearch.
 ];
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/facet.class.php';
